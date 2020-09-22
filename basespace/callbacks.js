091eed1a9ee1613ebd4b725961b04cf55cbaf7d5
John Garza <johnegarza@gmail.com>
2020-09-22 01:02:25 -0500

    Copy pasted the wrong value for docker image

--- basespace/callbacks.js
+++ basespace/callbacks.js
@@ -2,7 +2,7 @@ function launchSpec(dataProvider)
 {
     var ret = {
         commandLine: ['bash', '/opt/files/logging_helper.sh'],
-        containerImageId: "https://github.com/genome/docker-basespace_chromoseq",
+        containerImageId: "docker.illumina.com/mgibio/chromoseq@sha256:25559e850a21c3c9308ef145646a7e0327d8538704cd199fe45db72358e68a13",
         Options: [ "bsfs.enabled=true" ]
     };
     return ret;
